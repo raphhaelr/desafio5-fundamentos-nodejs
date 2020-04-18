f07d646c427431c5a393571cdcc7018dbcda4df9
Raphael <raphaelrbh7@gmail.com>
2020-04-18 11:37:47 -0300

    Desafio concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+/**
+ * Conexão com a persistência dos dados com as rotas.
+ * Realiza operações com o modelo como Create, Find, Delete, Update, etc.
+ */
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +25,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const valueIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((income, transaction) => {
+        return income + transaction.value;
+      }, 0);
+
+    const valueOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((outcome, transaction) => {
+        return outcome + transaction.value;
+      }, 0);
+
+    const balance = {
+      income: valueIncome,
+      outcome: valueOutcome,
+      total: valueIncome - valueOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
